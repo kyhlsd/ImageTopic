6d3f33db211fdd7edb8464a3702f6519c14c201e
kyhlsd <kmyghn@gmail.com>
2025-08-20 23:58:49 +0900

    Custom Error 생성

---
+++ ImageTopic/Network/APIError.swift
@@ -0,0 +1,31 @@
+//
+//  APIError.swift
+//  ImageTopic
+//
+//  Created by 김영훈 on 8/20/25.
+//
+
+import Foundation
+
+enum APIError: Int, LocalizedError {
+    case badRequest = 400
+    case unAuthorized = 401
+    case forbidden = 403
+    case notFound = 404
+    case unknown = 500
+    
+    var errorDescription: String? {
+        switch self {
+        case .badRequest:
+            return "잘못된 요청입니다."
+        case .unAuthorized:
+            return "유효하지 않은 토큰입니다."
+        case .forbidden:
+            return "권한이 없습니다."
+        case .notFound:
+            return "응답이 존재하지 않습니다."
+        case .unknown:
+            return "알 수 없는 오류가 발생했습니다."
+        }
+    }
+}
